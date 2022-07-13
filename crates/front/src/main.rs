04919ecd048905f1130725882af7f9ee139be996
interesnij <ochkarik1983@mail.ru>
2022-07-13 09:08:14 +0300

    J

--- crates/front/src/main.rs
+++ crates/front/src/main.rs
@@ -7,38 +7,41 @@ struct Video {
     speaker: String,
     url: String,
 }
-let videos = vec![
-    Video {
-        id: 1,
-        title: "Building and breaking things".to_string(),
-        speaker: "John Doe".to_string(),
-        url: "https://youtu.be/PsaFVLr8t4E".to_string(),
-    },
-    Video {
-        id: 2,
-        title: "The development process".to_string(),
-        speaker: "Jane Smith".to_string(),
-        url: "https://youtu.be/PsaFVLr8t4E".to_string(),
-    },
-    Video {
-        id: 3,
-        title: "The Web 7.0".to_string(),
-        speaker: "Matt Miller".to_string(),
-        url: "https://youtu.be/PsaFVLr8t4E".to_string(),
-    },
-    Video {
-        id: 4,
-        title: "Mouseless development".to_string(),
-        speaker: "Tom Jerry".to_string(),
-        url: "https://youtu.be/PsaFVLr8t4E".to_string(),
-    },
-];
+
 
 #[function_component(App)]
 fn app() -> Html {
+    let videos = vec![
+        Video {
+            id: 1,
+            title: "Building and breaking things".to_string(),
+            speaker: "John Doe".to_string(),
+            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
+        },
+        Video {
+            id: 2,
+            title: "The development process".to_string(),
+            speaker: "Jane Smith".to_string(),
+            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
+        },
+        Video {
+            id: 3,
+            title: "The Web 7.0".to_string(),
+            speaker: "Matt Miller".to_string(),
+            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
+        },
+        Video {
+            id: 4,
+            title: "Mouseless development".to_string(),
+            speaker: "Tom Jerry".to_string(),
+            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
+        },
+    ];
+    
     let videos = videos.iter().map(|video| html! {
         <p>{format!("{}: {}", video.speaker, video.title)}</p>
     }).collect::<Html>();
+
     html! {
         <>
         <h1>{ "RustConf Explorer" }</h1>
