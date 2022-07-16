81943a8312a75ac69661a3ddc0481a31f82a78f4
interesnij <ochkarik1983@mail.ru>
2022-07-16 12:10:17 +0300

    J

--- crates/communities/src/views/pages.rs
+++ crates/communities/src/views/pages.rs
@@ -1,5 +1,5 @@
-use diesel::prelude::*;
-use crate::schema;
+//use diesel::prelude::*;
+//use crate::schema;
 use actix_web::{
     HttpRequest,
     HttpResponse,
@@ -7,15 +7,15 @@ use actix_web::{
     web,
 };
 use serde::Deserialize;
-use crate::utils::{
-    establish_connection,
-};
+//use crate::utils::{
+//    establish_connection,
+//};
 
 pub fn pages_routes(config: &mut web::ServiceConfig) {
     config.route("/", web::get().to(index_page));
 }
 
-pub async fn index_page(req: HttpRequest) -> impl Responder {
+pub async fn index_page() -> impl Responder {
     HttpResponse::Ok().body(
         "<div style='background: #ccc;position:absolute;top:0;left:0;right:0;bottom:0'>
             <p style='text-align: center'>
