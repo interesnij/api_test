67e6b542524f0f2a55928fb995263c873481010c
interesnij <ochkarik1983@mail.ru>
2022-07-17 14:26:38 +0300

    'h

--- crates/users/src/handlers/user_handlers.rs
+++ crates/users/src/handlers/user_handlers.rs
@@ -54,7 +54,7 @@ async fn user_detail(_req: HttpRequest, _state: web::Data<AppState>, _token: Bea
     }
 }
 
-#[get("/{id}")]
+#[get("/{user_id}")]
 async fn user_profile(_state: web::Data<AppState>, user_id: web::Path<u64>) -> impl Responder{
     println!("user_profile!");
     let user: Result<User, _> = _state.rb.fetch_by_column("id", *user_id).await;
