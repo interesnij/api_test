dfd55a08672200a6adce4eceb053da3895ade854
interesnij <ochkarik1983@mail.ru>
2022-07-17 14:46:48 +0300

    'h

--- crates/users/src/main.rs
+++ crates/users/src/main.rs
@@ -53,6 +53,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         let cors = Cors::default()
             .allowed_origin("194.58.90.123:8000")
+            .allowed_origin("194.58.90.123:9001")
             .allowed_methods(vec!["GET", "POST"])
             .max_age(3600);
 
