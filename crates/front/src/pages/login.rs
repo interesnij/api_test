90883719859ddf7b03f51e1be2e5cccb92b6a809
interesnij <ochkarik1983@mail.ru>
2022-07-18 12:22:52 +0300

    J

--- crates/front/src/pages/login.rs
+++ crates/front/src/pages/login.rs
@@ -18,7 +18,7 @@ pub fn login_form() -> Html {
         let data_state = data_state.clone();
         async move {
             log::info!("data_state: {:?}", &*data_state.borrow_mut());
-            request_post::<UserLogin, UserToken>("/login".to_string(), &*data_state.borrow_mut())
+            request_post::<UserLogin, UserToken>("/api/users/login".to_string(), &*data_state.borrow_mut())
                 .await
         }
     });
