0571b35a696a230a6e33528739514e541d5e50d3
interesnij <ochkarik1983@mail.ru>
2022-07-18 14:20:36 +0300

    J

--- crates/front/src/utils/not_found.rs
+++ crates/front/src/utils/not_found.rs
@@ -11,9 +11,23 @@ use serde::Deserialize;
 use reqwasm::http::Request;
 
 #[derive(Clone, PartialEq, Deserialize, Debug)]
-struct Test {
-    text: String,
+struct UserDetail {
+    id: u64,
+    first_name: String,
+    last_name: String,
+    types: i16,
+    gender: String,
+    device: String,
+    language: String,
+    perm: i16,
+    link: String,
+    city: Option<String>,
+    status: Option<String>,
+    image: Option<String>,
+    birthday: String,
+    last_activity: String,
 }
+
 #[function_component(NotFound)]
 pub fn not_found() -> Html {
     let test = use_state(|| Test {text: "".to_string()});
@@ -22,7 +36,7 @@ pub fn not_found() -> Html {
         use_effect_with_deps(move |_| {
             let test = test.clone();
             wasm_bindgen_futures::spawn_local(async move {
-                let fetched_test: Test = Request::get("/api_users/v1/users/1")
+                let fetched_test: UserDetail = Request::get("/api_users/v1/users/1")
                     .send()
                     .await
                     .unwrap()
@@ -38,7 +52,7 @@ pub fn not_found() -> Html {
     html!{
         <section  class="section">
             <h2 class="title">{"404 Page not found"}</h2>
-            { test.text.clone() }
+            { test.id }
             <a href="/" class="button is-primary">{"back"}</a>
         </section>
     }
