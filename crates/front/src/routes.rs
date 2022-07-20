6e30f148b188eb139f2313391cc085b60f2d0e24
interesnij <ochkarik1983@mail.ru>
2022-07-20 18:39:01 +0300

    J

--- crates/front/src/routes.rs
+++ crates/front/src/routes.rs
@@ -2,10 +2,12 @@ use actix_web::web::ServiceConfig;
 
 use crate::views::{
     pages,
+    auth,
 };
 
 pub fn routes(cfg: &mut ServiceConfig) {
     cfg
     .configure(pages::pages_routes)
+    .configure(auth::auth_routes)
     ;
 }
